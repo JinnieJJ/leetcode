0b0e0e596e5a61845b00f05a8dc8fb4567ec47c1
Jingyi Jiang <susannahjiang@gmail.com>
2018-05-16 18:14:14 +0800

    Create combination sum.py

---
+++ combination sum.py
@@ -0,0 +1,22 @@
+class Solution:
+    def combinationSum(self, candidates, target):
+        """
+        :type candidates: List[int]
+        :type target: int
+        :rtype: List[List[int]]
+        """
+        results = []
+        self.Recursion(sorted(candidates), results, 0, [], target)
+        return results
+
+    def Recursion(self, candidates, results, start, solution, target):
+        if target == 0:
+            results.append(list(solution))
+            # print results
+        while start < len(candidates) and candidates[start] <= target:
+            solution.append(candidates[start])
+            self.Recursion(candidates, results, start, solution, target-candidates[start])
+            # print str(start)+":"
+            # print solution
+            solution.pop()
+            start += 1
