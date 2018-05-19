41fa5678df3da1b245a9ab301ba16e6ef7909d21
Jingyi Jiang <susannahjiang@gmail.com>
2018-05-20 00:57:27 +0800

    Create unique binary search tree.py

---
+++ unique binary search tree.py
@@ -0,0 +1,25 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def inorderTraversal(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[int]
+        """
+        result, stack = [], [(root, False)]
+        while stack:
+            root, is_visited = stack.pop()
+            if root is None:
+                continue
+            if is_visited:
+                result.append(root.val)
+            else:
+                stack.append((root.right, False))
+                stack.append((root, True))
+                stack.append((root.left, False))
+        return result
