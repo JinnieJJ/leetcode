f462c1009645b9f4d3a3cf3365e87381eda6fd78
Jingyi Jiang <susannahjiang@gmail.com>
2018-05-30 18:15:52 +0800

    Create majority element.py

---
+++ majority element.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def majorityElement(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        result = None
+        count = 0
+        for num in nums:
+            if count == 0:
+                result = num
+            if result == num:
+                count += 1
+            else:
+                count -= 1
+        return result
