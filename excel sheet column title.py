5b57da1dc6d7ebcb36b7a2f7f70db5bfa06dd3e1
Jingyi Jiang <susannahjiang@gmail.com>
2018-05-30 17:53:55 +0800

    Create excel sheet column title.py
    divmod
    chr

---
+++ excel sheet column title.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def convertToTitle(self, n):
+        """
+        :type n: int
+        :rtype: str
+        """
+        result = []
+        base = ord('A')
+        while n:
+            n, r = divmod(n - 1, 26)
+            result.append(chr(base + r))
+        return ''.join(result[::-1])
