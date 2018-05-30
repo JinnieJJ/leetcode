2691a281e135384140953512a33f798ea5fe71b1
Jingyi Jiang <susannahjiang@gmail.com>
2018-05-30 22:29:42 +0800

    Create excel sheet column number.py

---
+++ excel sheet column number.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def titleToNumber(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        base = ord('A') - 1
+        n = len(s)
+        result = 0
+        for i in range(n):
+            result += (ord(s[n - 1 - i]) - base) * pow(26, i)
+        return result
