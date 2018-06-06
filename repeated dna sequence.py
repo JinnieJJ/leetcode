038f092a95df68db6b5ff934eb9fed63fb47a704
Jingyi Jiang <susannahjiang@gmail.com>
2018-06-06 23:50:38 +0800

    Create repeated dna sequence.py
    由于直接将字符串存入字典会导致Memory Limit Exceeded，采用位操作将字符串转化为整数可以减少内存开销。

---
+++ repeated dna sequence.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def findRepeatedDnaSequences(self, s):
+        """
+        :type s: str
+        :rtype: List[str]
+        """
+        ans = []
+        valueCount = {}
+        map = {'A' : 0, 'C' : 1, 'G': 2, 'T' : 3}
+        sum = 0
+        for x in range(len(s)):
+            sum = (sum * 4 + map[s[x]]) & 0xFFFFF # sum*4 = sum << 2
+            if x < 9:
+                continue
+            valueCount[sum] = valueCount.get(sum, 0) + 1
+            if valueCount[sum] == 2:
+                ans.append(s[x - 9 : x + 1])
+        return ans
