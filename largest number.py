96ba1cfd4f1c47495cc2a0790fab2271f2c3f68f
Jingyi Jiang <susannahjiang@gmail.com>
2018-06-06 23:37:54 +0800

    Create largest number.py
    排序规则应该是如果AB>BA，那么我们规定A>B，也就是最后拼接时A放在B之前。
    这种比较关系具有传递性，第二是按照这样的拼接顺序得到的数是最大的。

---
+++ largest number.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def largestNumber(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: str
+        """
+        nums = [str(x) for x in nums]
+        nums.sort(cmp=lambda x, y: cmp(y + x, x + y))
+        largest = ''.join(nums)
+        return largest.lstrip('0') or '0'
