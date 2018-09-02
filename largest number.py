2a08dacbebab48211abe512f11e3cce2d577d699
Jingyi Jiang <susannahjiang@gmail.com>
2018-09-02 16:56:37 -0400

    Sep 2

--- largest number.py
+++ largest number.py
@@ -5,6 +5,6 @@ class Solution(object):
         :rtype: str
         """
         nums = [str(x) for x in nums]
-        nums.sort(cmp=lambda x, y: cmp(y + x, x + y))
+        nums.sort(cmp=lambda x, y: cmp(y + x, x + y)) # nums.sort(cmp=lambda x, y: int(y+x) - int(x+y))
         largest = ''.join(nums)
         return largest.lstrip('0') or '0'
