c33d0bb230db988decf9c828894e23b9469e15f4
Jingyi Jiang <susannahjiang@gmail.com>
2018-09-02 17:12:24 -0400

    Sep 2

--- 187-Repeated DNA Sequence.py
+++ 187-Repeated DNA Sequence.py
@@ -4,6 +4,7 @@ class Solution(object):
         :type s: str
         :rtype: List[str]
         """
+        
         ans = []
         valueCount = {}
         map = {'A' : 0, 'C' : 1, 'G': 2, 'T' : 3}
@@ -12,7 +13,10 @@ class Solution(object):
             sum = (sum * 4 + map[s[x]]) & 0xFFFFF # sum*4 = sum << 2
             if x < 9:
                 continue
-            valueCount[sum] = valueCount.get(sum, 0) + 1
+            if sum not in valueCount:
+                valueCount[sum] = 1
+            else:
+                valueCount[sum] += 1
             if valueCount[sum] == 2:
                 ans.append(s[x - 9 : x + 1])
         return ans
