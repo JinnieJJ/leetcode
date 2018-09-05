3e1a40748f60b6e2b771bd480dd3eed19d225915
Jingyi Jiang <susannahjiang@gmail.com>
2018-09-05 14:39:11 -0400

    Sep 5 FB

--- 91-Decode Ways.py
+++ 91-Decode Ways.py
@@ -6,7 +6,7 @@ class Solution:
         """
         if len(s) == 0:
             return 0
-        dp = [0 for _ in range(len(s))]
+        dp = [0 for _ in range(len(s) + 1)]
         dp[len(s)] = 1
         dp[len(s) - 1] = 1 if s[len(s) - 1] != '0' else 0
         for i in range(len(s)-2, -1, -1):
