65c3fd7a7280897ff908d6b23912feb04b8f1f1c
Jingyi Jiang <susannahjiang@gmail.com>
2018-09-14 15:30:59 -0400

    Sep 3, Sep 14 FB

--- 146-LRU Cache.py
+++ 146-LRU Cache.py
@@ -1,9 +1,10 @@
-class LRUCache(object):
+class LRUCache:
     class Node(object):
         def __init__(self, key, value):
             self.key = key
             self.value = value
-            self.prev, self.next = None, None
+            self.prev = None
+            self.next = None
 
     def __init__(self, capacity):
         """
@@ -12,32 +13,32 @@ class LRUCache(object):
         self.capacity = capacity
         self.size = 0
         self.dict = {}
-        self.head, self.tail = self.Node(-1, -1), self.Node(-1, -1)
+        self.head = self.Node(-1, -1)
+        self.tail = self.Node(-1, -1)
         self.head.next = self.tail
         self.tail.prev = self.head
-    
-    def _insert(self, node):
+        
+    def insert(self, node):
         node.prev, node.next = self.head, self.head.next
         self.head.next.prev = node
         self.head.next = node
-        
-    def _remove(self, node):
+    
+    def delete(self, node):
         node.prev.next = node.next
         node.next.prev = node.prev
-        node.prev = None
-        node.next = None
+        node.prev, node.next = None, None
 
     def get(self, key):
         """
         :type key: int
         :rtype: int
         """
-        if key not in self.dict:
-            return -1
-        node = self.dict[key]
-        self._remove(node)
-        self._insert(node)
-        return node.value
+        if key in self.dict:
+            node = self.dict[key]
+            self.delete(node)
+            self.insert(node)
+            return node.value
+        return -1
 
     def put(self, key, value):
         """
@@ -47,20 +48,19 @@ class LRUCache(object):
         """
         if key in self.dict:
             node = self.dict[key]
-            self._remove(node)
+            self.delete(node)
             node.value = value
-            self._insert(node)
+            self.insert(node)
         else:
-            if self.size == self.capacity:
+            if self.size >= self.capacity:
                 evict = self.tail.prev
-                self._remove(evict)
+                self.delete(evict)
                 del self.dict[evict.key]
                 self.size -= 1
             node = self.Node(key, value)
             self.dict[key] = node
-            self._insert(node)
+            self.insert(node)
             self.size += 1
-            
 
 
 # Your LRUCache object will be instantiated and called as such:
