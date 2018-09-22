473d9564e3d995e68c2a3700cb286282170ae5ae
Jingyi Jiang <susannahjiang@gmail.com>
2018-09-21 23:53:33 -0400

    Sep 21 Google

---
+++ 346-Moving Average from Data Stream.py
@@ -0,0 +1,29 @@
+class MovingAverage(object):
+
+    def __init__(self, size):
+        """
+        Initialize your data structure here.
+        :type size: int
+        """
+        self.size = size
+        self.sum = 0
+        self.q = collections.deque([])
+
+    def next(self, val):
+        """
+        :type val: int
+        :rtype: float
+        """
+        if len(self.q) == self.size:
+            self.sum -= self.q.popleft()
+        self.sum += val
+        self.q.append(val)
+        return float(self.sum) / len(self.q)
+
+
+        
+
+
+# Your MovingAverage object will be instantiated and called as such:
+# obj = MovingAverage(size)
+# param_1 = obj.next(val)
