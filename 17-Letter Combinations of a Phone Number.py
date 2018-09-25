e3db0331efda5526d602626caa47797e3d48ca72
Jingyi Jiang <susannahjiang@gmail.com>
2018-09-24 22:52:32 -0400

    Sep 4 FB, Sep 24 Dropbox

--- 17-Letter Combinations of a Phone Number.py
+++ 17-Letter Combinations of a Phone Number.py
@@ -20,5 +20,29 @@ class Solution:
                 result[i] = letters[int(i/n)] + result[i] # [d, e, f] [ad, ae, af, bd, be, bf, cd, ce, cf]
             
         return result
+    
+class Solution2(object):
+    def letterCombinations(self, digits):
+        """
+        :type digits: str
+        :rtype: List[str]
+        """
+        mapper = ["", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"]
+
+        if not digits:
+            return []
+
+        result = []
+        self.dfs(digits, mapper, result, 0, "")
+        return result
+
+    def dfs(self, digits, mapper, result, start, solution):
+        if start == len(digits):
+            result.append(solution)
+        else:
+            for c in mapper[int(digits[start])]:
+                solution += c
+                self.dfs(digits, mapper, result, start+1, solution)
+                solution = solution[:-1]
 
     
