95c556890a5af4b9d9f76ac80a6ad218ba52d196
Jingyi Jiang <susannahjiang@gmail.com>
2018-10-03 19:27:34 -0400

    Oct 3 Google

---
+++ 560-Subarray Sum Equals K.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def subarraySum(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: int
+        """
+        ans = sums = 0
+        cnt = collections.Counter()
+        for num in nums:
+            cnt[sums] += 1
+            sums += num
+            ans += cnt[sums - k]
+        return ans
