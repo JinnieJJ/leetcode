61bc64da82cbf396244585cadc301c5bbaffa43c
Jingyi Jiang <susannahjiang@gmail.com>
2018-10-05 10:19:37 -0400

    Oct 5 Google

---
+++ 737-Sentence Similarity II.py
@@ -0,0 +1,28 @@
+class Solution(object):
+    def areSentencesSimilarTwo(self, words1, words2, pairs):
+        """
+        :type words1: List[str]
+        :type words2: List[str]
+        :type pairs: List[List[str]]
+        :rtype: bool
+        """
+        if len(words1) != len(words2): return False
+        similars = collections.defaultdict(set)
+        for w1, w2 in pairs:
+            similars[w1].add(w2)
+            similars[w2].add(w1)
+
+        def dfs(words1, words2, visits):
+            for similar in similars[words2]:
+                if words1 == similar:
+                    return True
+                elif similar not in visits:
+                    visits.add(similar)
+                    if dfs(words1, similar, visits):
+                        return True
+            return False
+
+        for w1, w2 in zip(words1, words2):
+            if w1 != w2 and not dfs(w1, w2, set([w2])):
+                return False
+        return True
