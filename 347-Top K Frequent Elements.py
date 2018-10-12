7ad85a39a235bfcbbb066eb56e6d819475bd63fd
Jingyi Jiang <susannahjiang@gmail.com>
2018-10-12 00:30:58 -0400

    Oct 11 FB

---
+++ 347-Top K Frequent Elements.py
@@ -0,0 +1,11 @@
+import heapq
+class Solution(object):
+    def topKFrequent(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: List[int]
+        """
+        c = collections.Counter(nums)
+        ans = [x for x,y in c.most_common(k)]
+        return ans
