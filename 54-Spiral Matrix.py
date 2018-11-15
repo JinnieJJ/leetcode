e13dc952a9aae449c880db002fd5bd215a2df70f
Jingyi Jiang <susannahjiang@gmail.com>
2018-11-15 12:02:00 -0500

    Nov 15 Uber

--- 54-Spiral Matrix.py
+++ 54-Spiral Matrix.py
@@ -1,4 +1,4 @@
-class Solution:
+class Solution(object):
     def spiralOrder(self, matrix):
         """
         :type matrix: List[List[int]]
@@ -13,17 +13,17 @@ class Solution:
         top = 0
         bottom = len(matrix) - 1
         
-        while left<=right and top<=bottom:
-            for i in range(left, right+1):
-                result.append(matrix[top][i])
-            for j in range(top+1, bottom):
-                result.append(matrix[j][right])
-            for k in reversed(range(left, right+1)):
+        while left <= right and top <= bottom:
+            for j in range(left, right+1):
+                result.append(matrix[top][j])
+            for i in range(top+1, bottom):
+                result.append(matrix[i][right])
+            for j in reversed(range(left, right+1)):
                 if top < bottom:
-                    result.append(matrix[bottom][k])
-            for m in reversed(range(top+1, bottom)):
+                    result.append(matrix[bottom][j])
+            for i in reversed(range(top+1, bottom)):
                 if left < right:
-                    result.append(matrix[m][left])
+                    result.append(matrix[i][left])
             left += 1
             right -= 1
             top += 1
