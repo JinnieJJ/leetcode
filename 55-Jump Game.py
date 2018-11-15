fb27ee6c1863e36dbce15ba7dd3507b3a79232cc
Jingyi Jiang <susannahjiang@gmail.com>
2018-11-15 13:16:27 -0500

    Nov 15 Uber

--- 55-Jump Game.py
+++ 55-Jump Game.py
@@ -1,13 +1,12 @@
-class Solution:
+class Solution(object):
     def canJump(self, nums):
         """
         :type nums: List[int]
         :rtype: bool
         """
         count = 0
-        
-        for i,x in enumerate(nums):
+        for i, step in enumerate(nums):
             if i > count:
                 break
-            count = max(count, i+x)
+            count = max(count, i + step)
         return count >= len(nums) - 1
