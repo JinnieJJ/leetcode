7b118c003ff5385e7915404d8fa73f0063e697da
Jingyi Jiang <susannahjiang@gmail.com>
2019-05-22 01:09:33 -0700

    May 21 2019 DP

---
+++ 377-Combination Sum IV.go
@@ -0,0 +1,15 @@
+import "sort"
+func combinationSum4(nums []int, target int) int {
+    sort.Ints(nums)
+    dp := make([]int, target+1)
+    dp[0] = 1
+    for i := 0; i <= target; i++  {
+        for _, num := range nums {
+            if num > i {
+                break
+            }
+            dp[i] += dp[i-num]
+        }
+    }
+    return dp[target]
+}
