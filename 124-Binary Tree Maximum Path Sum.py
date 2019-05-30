92d902605053a76ad76344b48d7d0644f292c1a5
Jingyi Jiang <susannahjiang@gmail.com>
2019-05-29 22:39:52 -0700

    May 29 2019 Tree

--- 124-Binary Tree Maximum Path Sum.py
+++ 124-Binary Tree Maximum Path Sum.py
@@ -11,15 +11,16 @@ class Solution(object):
         :type root: TreeNode
         :rtype: int
         """
-        self.ans = None
-        
-        def search(root):
-            if root is None:
-                return 0
-            leftMax = search(root.left)
-            rightMax = search(root.right)
-            self.ans = max(max(leftMax, 0) + max(rightMax, 0) + root.val, self.ans)
-            return max(leftMax, rightMax, 0) + root.val
-        
-        search(root)
-        return self.ans
+        self.max_sum = float('-inf')
+        self.max_gain(root)
+        return self.max_sum
+    
+    def max_gain(self, node):
+        if not node:
+            return 0
+        left_gain = max(self.max_gain(node.left), 0)
+        right_gain = max(self.max_gain(node.right), 0)
+        price_newpath = node.val + left_gain + right_gain
+        self.max_sum = max(self.max_sum, price_newpath)
+        return node.val + max(left_gain, right_gain)
+    
