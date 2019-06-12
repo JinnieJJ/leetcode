bde0871be7e159db216ce9ec15ba4a634941b724
Jingyi Jiang <susannahjiang@gmail.com>
2019-06-11 22:18:41 -0700

    Jun 11 2019 Tree

--- 113-Path Sum II.py
+++ 113-Path Sum II.py
@@ -1,11 +1,11 @@
 # Definition for a binary tree node.
-# class TreeNode:
+# class TreeNode(object):
 #     def __init__(self, x):
 #         self.val = x
 #         self.left = None
 #         self.right = None
 
-class Solution:
+class Solution(object):
     def pathSum(self, root, sum):
         """
         :type root: TreeNode
@@ -13,16 +13,16 @@ class Solution:
         :rtype: List[List[int]]
         """
         result = []
-        self.Recursion(root, sum, [], result)
+        self.recursionHelper(root, sum, [], result)
         return result
-
-    def Recursion(self, root, sum, curr, result):
+    
+    def recursionHelper(self, root, sum, cur, result):
         if not root:
             return
         sum -= root.val
-        if sum == 0 and root.left is None and root.right is None:
-            result.append(curr + [root.val])
+        if sum == 0 and not root.left and not root.right:
+            result.append(cur+[root.val])
         if root.left:
-            self.Recursion(root.left, sum, curr + [root.val], result)
+            self.recursionHelper(root.left, sum, cur+[root.val], result)
         if root.right:
-            self.Recursion(root.right, sum, curr + [root.val], result)
+            self.recursionHelper(root.right, sum, cur+[root.val], result)
