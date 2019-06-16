97bceb02c79dbccd8da78c168c352dbc4a1deef5
Jingyi Jiang <susannahjiang@gmail.com>
2019-06-16 13:22:14 -0700

    Jun 16 2019 Tree

---
+++ 513-Find Bottom Left Tree Value.py
@@ -0,0 +1,27 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def findBottomLeftValue(self, root):
+        """
+        :type root: TreeNode
+        :rtype: int
+        """
+        if not root:
+            return root
+        q = collections.deque([root])
+        while q:
+            nodes = []
+            for _ in range(len(q)):
+                node = q.popleft()
+                if node.left:
+                    q.append(node.left)
+                if node.right:
+                    q.append(node.right)
+                if not node.left and not node.right:
+                    nodes.append(node)
+        return nodes[0].val
