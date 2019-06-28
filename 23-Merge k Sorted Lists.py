96ea0286f1a99d15ad7dac9de067c2a6be965e37
Jingyi Jiang <susannahjiang@gmail.com>
2019-06-28 11:44:39 -0700

    Jun 28 2019 List

--- 23-Merge k Sorted Lists.py
+++ 23-Merge k Sorted Lists.py
@@ -1,26 +1,28 @@
-from Queue import PriorityQueue
+import heapq
 
 # Definition for singly-linked list.
-# class ListNode:
+# class ListNode(object):
 #     def __init__(self, x):
 #         self.val = x
 #         self.next = None
 
-class Solution:
+class Solution(object):
     def mergeKLists(self, lists):
         """
         :type lists: List[ListNode]
         :rtype: ListNode
         """
-        dummy = ListNode(None)
-        curr = dummy
-        q = PriorityQueue()
+        result = ListNode(-1)
+        cur = result
+        
+        h = []
         for node in lists:
-            if node: 
-                q.put((node.val,node))
-        while q.qsize() > 0:
-            curr.next = q.get()[1]
-            curr = curr.next
-            if curr.next: 
-                q.put((curr.next.val, curr.next))
-        return dummy.next
+            if node:
+                heapq.heappush(h, (node.val, node))
+        
+        while len(h) > 0:
+            cur.next = heapq.heappop(h)[1]
+            cur = cur.next
+            if cur.next:
+                heapq.heappush(h, (cur.next.val, cur.next))
+        return result.next
