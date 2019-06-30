f7b7a840523b81953536575e791af26bf1242bf0
Jingyi Jiang <susannahjiang@gmail.com>
2019-06-30 14:21:39 -0700

    Jun 30 2019 List

--- 24-Swap Nodes in Pairs.py
+++ 24-Swap Nodes in Pairs.py
@@ -1,24 +1,26 @@
 # Definition for singly-linked list.
-# class ListNode:
+# class ListNode(object):
 #     def __init__(self, x):
 #         self.val = x
 #         self.next = None
 
-class Solution:
+class Solution(object):
     def swapPairs(self, head):
         """
         :type head: ListNode
         :rtype: ListNode
         """
-        result = ListNode(0)
+        result = ListNode(-1)
         result.next = head
         tmp = result
-        while tmp.next and tmp.next.next:
-            node1 = tmp.next
+        while tmp and tmp.next and tmp.next.next:
+            node1 = tmp.next.next.next
             node2 = tmp.next.next
-            node3 = tmp.next.next.next
+            node3 = tmp.next
+            node2.next = None
+            node3.next = None
             tmp.next = node2
-            node2.next = node1
-            node1.next = node3
-            tmp = node1
+            node2.next = node3
+            node3.next = node1
+            tmp = node3
         return result.next
