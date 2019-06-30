83c163794162d886c4c42b70e32fd41bf7f01b6b
Jingyi Jiang <susannahjiang@gmail.com>
2019-06-30 16:51:09 -0700

    Jun 30 2019 List

--- convert sorted list to binary search tree.py
+++ convert sorted list to binary search tree.py
@@ -1,36 +1,41 @@
 # Definition for singly-linked list.
-# class ListNode:
+# class ListNode(object):
 #     def __init__(self, x):
 #         self.val = x
 #         self.next = None
 
 # Definition for a binary tree node.
-# class TreeNode:
+# class TreeNode(object):
 #     def __init__(self, x):
 #         self.val = x
 #         self.left = None
 #         self.right = None
 
-class Solution:
+class Solution(object):
     def sortedListToBST(self, head):
         """
         :type head: ListNode
         :rtype: TreeNode
         """
-        node, length = head, 0
-        while node:
-            node = node.next
-            length += 1
-        self.curr = head
-        return self.Recursion(0, length - 1)
-
-    def Recursion(self, left, right):
-        if left > right:
+        if not head:
             return None
-        mid = (left + right) // 2
-        left = self.Recursion(left, mid - 1)
-        root = TreeNode(self.curr.val)
-        root.left = left
-        self.curr = self.curr.next
-        root.right = self.Recursion(mid + 1, right)
-        return root
+        if not head.next:
+            return TreeNode(head.val)
+        mid = self.findMiddle(head)
+        node = TreeNode(mid.val)
+        node.left = self.sortedListToBST(head)
+        node.right = self.sortedListToBST(mid.next)
+        return node
+        
+    
+    def findMiddle(self, head):
+        prev = None
+        slow = head
+        fast = head
+        while fast and fast.next:
+            prev = slow
+            slow = slow.next
+            fast = fast.next.next
+        if prev:
+            prev.next = None
+        return slow
