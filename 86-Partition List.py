fa6f657f9c72c3b3276a665151a1cba8584bd77c
Jingyi Jiang <susannahjiang@gmail.com>
2019-06-30 16:39:11 -0700

    Jun 30 2019 List

---
+++ 86-Partition List.py
@@ -0,0 +1,28 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def partition(self, head, x):
+        """
+        :type head: ListNode
+        :type x: int
+        :rtype: ListNode
+        """
+        small = ListNode(-1)
+        big = ListNode(-1)
+        smallHead = small
+        bigHead = big
+        while head:
+            if head.val < x:
+                small.next = head
+                small = small.next
+            else:
+                big.next = head
+                big = big.next
+            head = head.next
+        big.next = None
+        small.next = bigHead.next
+        return smallHead.next
