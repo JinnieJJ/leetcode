55f9564c4e117c0c032e1a44e01062165c961309
Jingyi Jiang <susannahjiang@gmail.com>
2019-06-30 13:57:36 -0700

    Jun 30 2019 List

--- 138-Copy List with Random Pointer.py
+++ 138-Copy List with Random Pointer.py
@@ -25,3 +25,42 @@ class Solution(object):
             visited[node].random = visited[node.random] if node.random else None
             node = node.next
         return visited[head]
+    
+    """
+# Definition for a Node.
+class Node(object):
+    def __init__(self, val, next, random):
+        self.val = val
+        self.next = next
+        self.random = random
+"""
+class Solution(object):
+    def copyRandomList(self, head):
+        """
+        :type head: Node
+        :rtype: Node
+        """
+        if not head:
+            return head
+        
+        node = head
+        while node:
+            copyNode = Node(node.val, node.next, None)
+            node.next = copyNode
+            node = copyNode.next
+        
+        node = head
+        while node:
+            node.next.random = node.random.next if node.random else None
+            node = node.next.next
+        
+        oldPtr = head
+        newPtr = head.next
+        copyHead = head.next
+        while oldPtr:
+            oldPtr.next = oldPtr.next.next
+            newPtr.next = newPtr.next.next if newPtr.next else None
+            oldPtr = oldPtr.next
+            newPtr = newPtr.next
+        
+        return copyHead
