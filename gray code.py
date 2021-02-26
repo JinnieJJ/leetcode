477a1f26fb20e873602e64d47d9bddad65609047
JinnieJJ <susannahjiang@gmail.com>
2021-02-25 21:50:44 -0500

    Remove redundant files

--- gray code.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    def grayCode(self, n):
-        """
-        :type n: int
-        :rtype: List[int]
-        """
-        return [(i >> 1) ^ i for i in range(pow(2, n))]
-        # G(N) = B(n/2) XOR B(n)
